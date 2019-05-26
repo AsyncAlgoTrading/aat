dae5f7014c966cf7e6f139ff13101706c3f40b39
Tim Paine <t.paine154@gmail.com>
2019-05-26 11:56:47 -0400

    transition to cmake

--- setup.py
+++ setup.py
@@ -1,27 +1,83 @@
 # # # # GENERATED FILE -- DO NOT MODIFY # # # #
-from setuptools import setup, find_packages
-from distutils.extension import Extension
+from setuptools import setup, find_packages, Extension
+from distutils.version import LooseVersion
 from codecs import open
+from setuptools.command.build_ext import build_ext
 import os
 import os.path
+import os
+import re
+import sys
+import platform
+import subprocess
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-sources = []
-outputs = []
-for path, subdirs, files in os.walk('aat/src'):
-    for name in files:
-        fp = os.path.join(path, name)
-        if fp.endswith('cpp'):
-            outputs.append(fp.replace('.cpp', '').replace('/src', '').replace('/', '.'))
-            sources.append(fp)
-print(sources)
-print(outputs)
 with open(os.path.join(here, 'requirements.txt'), encoding='utf-8') as f:
     requires = f.read().split()
+
+if sys.version_info.major < 3 or sys.version_info.minor < 7:
+    raise Exception('Must be python3.7 or above')
+
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError(
+                "CMake must be installed to build the following extensions: " +
+                ", ".join(e.name for e in self.extensions))
+
+        if platform.system() == "Windows":
+            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)',
+                                         out.decode()).group(1))
+            if cmake_version < '3.1.0':
+                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(
+            os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
+
+        if platform.system() == "Windows":
+            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(
+                cfg.upper(),
+                extdir)]
+            if sys.maxsize > 2**32:
+                cmake_args += ['-A', 'x64']
+            build_args += ['--', '/m']
+        else:
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+            build_args += ['--', '-j4']
+
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
+            env.get('CXXFLAGS', ''),
+            self.distribution.get_version())
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args,
+                              cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args,
+                              cwd=self.build_temp)
+        print()  # Add an empty line for cleaner output
 
 setup(
     name='aat',
@@ -34,7 +90,7 @@ setup(
     author_email='timothy.k.paine@gmail.com',
     license='Apache 2.0',
     install_requires=requires,
-
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 2',
@@ -57,12 +113,8 @@ setup(
             'aat=aat:main',
         ],
     },
-
     ext_modules=[
-        Extension(x,
-                  include_dirs=["aat/include"],
-                  sources=[y],
-                  libraries=["boost_python"]) for x, y in zip(outputs, sources)
-    ]
-
+        CMakeExtension('aat'),
+    ],
+    cmdclass=dict(build_ext=CMakeBuild),
 )
