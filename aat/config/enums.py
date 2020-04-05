e9f80089241b17a34bef6b0b2ebaa24f275c928c
Tim Paine <t.paine154@gmail.com>
2020-04-05 17:52:40 -0400

    begining to move to c++
    .
    
    .
    
    .

--- aat/config/enums.py
+++ aat/config/enums.py
@@ -1,5 +1,5 @@
 from enum import Enum
-# from ._enums import EventType, DataType
+from ..binding import Side, InstrumentType, EventType, DataType  # noqa: F401
 
 
 class BaseEnum(Enum):
@@ -13,48 +13,6 @@ class BaseEnum(Enum):
 
     def __str__(self):
         return f'{self.value}'
-
-
-class InstrumentType(BaseEnum):
-    CURRENCY = 'CURRENCY'
-    # PAIR = 'PAIR'
-    EQUITY = 'EQUITY'
-    # BOND = 'BOND'
-    # OPTION = 'OPTION'
-    # FUTURE = 'FUTURE'
-
-
-class Side(BaseEnum):
-    BUY = 'BUY'
-    SELL = 'SELL'
-
-
-class EventType(BaseEnum):
-    # Trade events
-    TRADE = 'TRADE'
-
-    # Order events
-    OPEN = 'OPEN'
-    CANCEL = 'CANCEL'
-    CHANGE = 'CHANGE'
-    FILL = 'FILL'
-
-    # Other data events
-    DATA = 'DATA'
-
-    # System events
-    HALT = 'HALT'
-    CONTINUE = 'CONTINUE'
-
-    # Engine events
-    ERROR = 'ERROR'
-    START = 'START'
-    EXIT = 'EXIT'
-
-
-class DataType(BaseEnum):
-    ORDER = 'ORDER'
-    TRADE = 'TRADE'
 
 
 class OrderType(BaseEnum):
--- cpp/include/aat/binding.hpp
+++ cpp/include/aat/binding.hpp
@@ -39,26 +39,11 @@ PYBIND11_MODULE(binding, m) {
         .value("STOP_LIMIT", OrderType::STOP_LIMIT)
         .value("STOP_MARKET", OrderType::STOP_MARKET);
 
-    py::bind_vector<std::vector<std::string>>(m, "StringVec");
-    m.attr("Sides") = Side_names;
-    m.attr("EventTypes") = EventType_names;
-    m.attr("DataTypes") = DataType_names;
-    m.attr("InstrumentTypes") = InstrumentType_names;
-    m.attr("OrderTypes") = OrderType_names;
-    m.attr("OrderFlags") = OrderFlag_names;
-
-    m.def("Side_to_string", &Side_to_string, "Side enum to string");
-    m.def("EventType_to_string", &EventType_to_string, "EventType enum to string");
-    m.def("DataType_to_string", &DataType_to_string, "DataType enum to string");
-    m.def("InstrumentType_to_string", &InstrumentType_to_string, "InstrumentType enum to string");
-    m.def("OrderType_to_string", &OrderType_to_string, "OrderType enum to string");
-    m.def("OrderFlag_to_string", &OrderFlag_to_string, "OrderFlag enum to string");
-    m.def("Side_from_string", &Side_from_string, "string to Side enum");
-    m.def("EventType_from_string", &EventType_from_string, "string to EventType enum");
-    m.def("DataType_from_string", &DataType_from_string, "string to DataType enum");
-    m.def("InstrumentType_from_string", &InstrumentType_from_string, "string to InstrumentType enum");
-    m.def("OrderType_from_string", &OrderType_from_string, "string to OrderType enum");
-    m.def("OrderFlag_from_string", &OrderFlag_from_string, "string to OrderFlag enum");
+    py::enum_<OrderFlag>(m, "OrderFlag", py::arithmetic())
+        .value("NONE", OrderFlag::NONE)
+        .value("FILL_OR_KILL", OrderFlag::FILL_OR_KILL)
+        .value("ALL_OR_NONE", OrderFlag::ALL_OR_NONE)
+        .value("IMMEDIATE_OR_CANCEL", OrderFlag::IMMEDIATE_OR_CANCEL);
 
     using namespace aat::core;
     py::class_<OrderBook>(m, "OrderBook")
