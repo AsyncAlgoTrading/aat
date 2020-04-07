c226c0f0e17372b51a97e40d8f3a3b8e8bb9787f
Tim Paine <t.paine154@gmail.com>
2020-04-07 16:46:51 -0400

    add enum tests

--- aat/tests/config/test_enums.py
+++ aat/tests/config/test_enums.py
@@ -18,3 +18,26 @@ class TestEnums:
         assert EventType.ERROR.name == "ERROR"
         assert EventType.START.name == "START"
         assert EventType.EXIT.name == "EXIT"
+
+    def test_data_type(self):
+        from aat.binding import DataType
+        assert DataType.ORDER.name == "ORDER"
+        assert DataType.TRADE.name == "TRADE"
+
+    def test_instrument_type(self):
+        from aat.binding import InstrumentType
+        assert InstrumentType.CURRENCY.name == "CURRENCY"
+        assert InstrumentType.EQUITY.name == "EQUITY"
+
+    def test_order_type(self):
+        from aat.binding import OrderType
+        assert OrderType.LIMIT.name == "LIMIT"
+        assert OrderType.MARKET.name == "MARKET"
+        assert OrderType.STOP.name == "STOP"
+
+    def test_order_flag(self):
+        from aat.binding import OrderFlag
+        assert OrderFlag.NONE.name == "NONE"
+        assert OrderFlag.FILL_OR_KILL.name == "FILL_OR_KILL"
+        assert OrderFlag.ALL_OR_NONE.name == "ALL_OR_NONE"
+        assert OrderFlag.IMMEDIATE_OR_CANCEL.name == "IMMEDIATE_OR_CANCEL"
