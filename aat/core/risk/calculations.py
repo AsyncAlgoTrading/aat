f92c30c8e47adc188609ab5d629021c786406507
Tim Paine <t.paine154@gmail.com>
2020-08-02 22:20:41 -0400

    separate functions

--- aat/core/risk/calculations.py
+++ aat/core/risk/calculations.py
@@ -142,10 +142,10 @@ class CalculationsMixin(object):
         self._df_pnl = self._getPnl()
         self._df_pnl.fillna(0.0, inplace=True)
 
+        self._df_price = self._getPrice()
+
         self._df_total_pnl = self._df_pnl[[c for c in self._df_pnl if 'pnl:' in c]]
         self._df_total_pnl.columns = [c.replace('pnl:', '') for c in self._df_total_pnl.columns]
-
-        self._df_price = self._getPrice()
 
         self._df_size = self._getSize()
         self._df_size.columns = [c.replace('s:', '') for c in self._df_size.columns]
@@ -155,6 +155,106 @@ class CalculationsMixin(object):
 
         self._df_investment = self._getInvestment()
         self._df_investment.columns = [c.replace('i:', '') for c in self._df_investment.columns]
+
+    def plotPrice(self, ax=None, **plot_kwargs):
+        self._df_price = self._getPrice()
+        self._df_price.plot(ax=ax, **plot_kwargs)
+        if ax:
+            ax.set_ylabel('Price')
+
+    def plotPositions(self, ax=None, **plot_kwargs):
+        self._df_size = self._getSize()
+        self._df_size.columns = [c.replace('s:', '') for c in self._df_size.columns]
+
+        self._df_size.plot(kind='area', ax=ax, stacked=True, linewidth=0, **plot_kwargs)
+        if ax:
+            ax.set_ylabel('Positions')
+
+    def plotNotional(self, ax=None, **plot_kwargs):
+        df_position_notional = self._getSize()
+        df_position_notional.columns = [c.replace('s:', '') for c in self._df_size.columns]
+
+        for col in df_position_notional.columns:
+            df_position_notional[col] = df_position_notional[col] * self._df_price[col]
+
+        df_position_notional.fillna(method='ffill', inplace=True)
+        df_position_notional.plot(kind='area', ax=ax, stacked=True, linewidth=0, **plot_kwargs)
+        if ax:
+            ax.set_ylabel('Notional')
+
+    def plotPnl(self, ax=None, **plot_kwargs):
+        # cm = matplotlib.cm.get_cmap("Paired")
+        # ls = np.linspace(0, 1, len(pnl_cols) // 2)
+        # colors = []
+        # for i in ls:
+        #     colors.extend([matplotlib.colors.to_hex(cm(i)), matplotlib.colors.to_hex(cm(i + .05))])
+        self._df_pnl = self._getPnl()
+        self._df_pnl.fillna(0.0, inplace=True)
+
+        self._df_total_pnl = self._df_pnl[[c for c in self._df_pnl if 'pnl:' in c]]
+        self._df_total_pnl.columns = [c.replace('pnl:', '') for c in self._df_total_pnl.columns]
+        self._df_total_pnl.plot(ax=ax)
+        if ax:
+            ax.set_ylabel('PNL')
+
+    def plotUpDown(self, ax=None, **plot_kwargs):
+        self._df_pnl = self._getPnl()
+        self._df_pnl.fillna(0.0, inplace=True)
+
+        df2 = self._df_pnl[['alpha']]
+        df2['pos'] = df2['alpha']
+        df2['neg'] = df2['alpha']
+        df2['pos'][df2['pos'] <= 0] = np.nan
+        df2['neg'][df2['neg'] > 0] = np.nan
+        df2.plot(ax=ax, y=['pos', 'neg'], kind='area', stacked=False, color=['green', 'red'], legend=False, linewidth=0, fontsize=5, rot=0, **plot_kwargs)
+        if ax:
+            ax.set_ylabel('Alpha')
+
+    def plotReturnHistograms(self, **plot_kwargs):
+        self._df_notional = self._getNotional()
+        self._df_notional.columns = [c.replace('n:', '') for c in self._df_notional.columns]
+
+        df_returns = []
+        for col in self._df_notional.columns:
+            self._df_notional[col].drop_duplicates().pct_change(1).fillna(0.0)
+            # drop if exactly -100% (e.g. "sold")
+            df_returns.append(self._df_notional[col].drop_duplicates().pct_change(1).fillna(0.0))
+
+        df_returns = pd.concat(df_returns, axis=1, sort=True)
+        df_returns.sort_index(inplace=True)
+        df_returns = df_returns.groupby(df_returns.index).last()
+        df_returns.drop_duplicates(inplace=True)
+
+        fig2 = plt.figure(figsize=(9, 5))
+        grid = plt.GridSpec(2, len(df_returns.columns), figure=fig2, wspace=0.4, hspace=0.3)
+        axes2 = []
+        for _ in range(len(df_returns.columns)):
+            axes2.append(plt.subplot(grid[0, _]))
+
+        for i, col in enumerate(df_returns.columns):
+            df_returns.hist(column=col, ax=axes2[i], grid=False)
+
+    def plotStdDev(self, ax, **plot_kwargs):
+        self._df_notional = self._getNotional()
+        self._df_notional.columns = [c.replace('n:', '') for c in self._df_notional.columns]
+
+        total_returns = self._df_notional.sum(axis=1).pct_change(1).fillna(0.0)
+        total_returns_rolling = total_returns.rolling(10)
+        total_returns_rolling.std().plot(ax=ax)
+        ax.axhline(total_returns.std())
+        ax.set_ylabel('Std.')
+
+    def plotSharpe(self, ax, **plot_kwargs):
+        self._df_notional = self._getNotional()
+        self._df_notional.columns = [c.replace('n:', '') for c in self._df_notional.columns]
+
+        total_returns = self._df_notional.sum(axis=1).pct_change(1).fillna(0.0)
+
+        sharpe = total_returns.values.mean() / total_returns.values.std() * np.sqrt(252)
+        total_returns['sharpe'] = total_returns.rolling(10).mean() / total_returns.rolling(10).std() * np.sqrt(252)
+        total_returns['sharpe'].plot(ax=ax)
+        ax.axhline(sharpe)
+        ax.set_ylabel('Sharpe')
 
     def performanceCharts(self):
         self.collectStats()
@@ -169,74 +269,28 @@ class CalculationsMixin(object):
         plt.rc('legend', fontsize=4)  # using a size in points
 
         # Plot prices
-        self._df_price.plot(ax=axes[0])
-        axes[0].set_ylabel('Price')
+        self.plotPrice(ax=axes[0])
 
         # Plot Positions
-        self._df_size.plot(kind='area', ax=axes[1], stacked=True, linewidth=0)
-        axes[1].set_ylabel('Positions')
+        self.plotPositions(ax=axes[1])
 
-        df_position_notional = self._df_size.copy()
-        for col in df_position_notional.columns:
-            df_position_notional[col] = df_position_notional[col] * self._df_price[col]
-
-        df_position_notional.fillna(method='ffill', inplace=True)
-        df_position_notional.plot(kind='area', ax=axes[2], stacked=True, linewidth=0)
-        axes[2].set_ylabel('Notional')
+        # Plot Notionals
+        self.plotNotional(ax=axes[2])
 
         # Plot PNLs
-        # cm = matplotlib.cm.get_cmap("Paired")
-        # ls = np.linspace(0, 1, len(pnl_cols) // 2)
-        # colors = []
-        # for i in ls:
-        #     colors.extend([matplotlib.colors.to_hex(cm(i)), matplotlib.colors.to_hex(cm(i + .05))])
-
-        self._df_total_pnl.plot(ax=axes[3])
-        axes[3].set_ylabel('PNL')
+        self.plotPnl(ax=axes[3])
 
         # Plot up/down chart
-        df2 = self._df_pnl[['alpha']]
-        df2['pos'] = df2['alpha']
-        df2['neg'] = df2['alpha']
-        df2['pos'][df2['pos'] <= 0] = np.nan
-        df2['neg'][df2['neg'] > 0] = np.nan
-        df2.plot(ax=axes[4], y=['pos', 'neg'], kind='area', stacked=False, color=['green', 'red'], legend=False, linewidth=0, fontsize=5, rot=0)
-        axes[4].set_ylabel('Alpha')
-
-        # Plot returns
-        df_returns = []
-        for col in self._df_notional.columns:
-            self._df_notional[col].drop_duplicates().pct_change(1).fillna(0.0)
-            # drop if exactly -100% (e.g. "sold")
-            df_returns.append(self._df_notional[col].drop_duplicates().pct_change(1).fillna(0.0))
-
-        df_returns = pd.concat(df_returns, axis=1, sort=True)
-        df_returns.sort_index(inplace=True)
-        df_returns = df_returns.groupby(df_returns.index).last()
-        df_returns.drop_duplicates(inplace=True)
+        self.plotUpDown(ax=axes[4])
 
         # rolling stddev
-        total_returns = self._df_notional.sum(axis=1).pct_change(1).fillna(0.0)
-        total_returns_rolling = total_returns.rolling(10)
-        total_returns_rolling.std().plot(ax=axes[5])
-        axes[5].axhline(total_returns.std())
-        axes[5].set_ylabel('Std.')
+        self.plotStdDev(ax=axes[5])
+        self.plotSharpe(ax=axes[6])
 
-        sharpe = total_returns.values.mean() / total_returns.values.std() * np.sqrt(252)
-        total_returns['sharpe'] = total_returns.rolling(10).mean() / total_returns.rolling(10).std() * np.sqrt(252)
-        total_returns['sharpe'].plot(ax=axes[6])
-        axes[6].axhline(sharpe)
-        axes[6].set_ylabel('Sharpe')
+        # Plot returns
+        self.plotReturnHistograms()
 
-        fig2 = plt.figure(figsize=(9, 5))
-        grid = plt.GridSpec(2, len(df_returns.columns), figure=fig2, wspace=0.4, hspace=0.3)
-        axes2 = []
-        for _ in range(len(df_returns.columns)):
-            axes2.append(plt.subplot(grid[0, _]))
-
-        for i, col in enumerate(df_returns.columns):
-            df_returns.hist(column=col, ax=axes2[i], grid=False)
-
+        # Show plot
         plt.show()
 
     def ipython(self):
