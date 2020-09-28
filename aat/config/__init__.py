d0ebf73bc89989bb444f82911f08833f8fd88a30
Tim Paine <t.paine154@gmail.com>
2020-09-27 21:41:20 -0400

    fix lint

--- aat/config/__init__.py
+++ aat/config/__init__.py
@@ -2,13 +2,13 @@ from ..common import _in_cpp
 from .parser import parseConfig, getStrategies, getExchanges  # noqa: F401
 
 if _in_cpp():
-    from ..binding import (TradingTypeCpp as TradingType,  # type: ignore 
+    from ..binding import (TradingTypeCpp as TradingType,  # type: ignore
                            SideCpp as Side,
                            InstrumentTypeCpp as InstrumentType,
                            EventTypeCpp as EventType,
                            DataTypeCpp as DataType,
                            OrderTypeCpp as OrderType,
-                           OrderFlagCpp as OrderFlag,  
+                           OrderFlagCpp as OrderFlag,
                            ExitRoutineCpp as ExitRoutine)  # noqa: F401
 else:
     from .enums import TradingType, Side, InstrumentType, EventType, DataType, OrderFlag, OrderType, OptionType, ExitRoutine  # noqa: F401
