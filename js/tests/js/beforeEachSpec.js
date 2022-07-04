d946e99751336db794d7330360109f922c4c25d9
Tim Paine <t.paine154@gmail.com>
2022-07-03 21:15:40 -0400

    remove js files for now, remove docker, consolidate bumpversion config, update makefile for javascript

--- js/tests/js/beforeEachSpec.js
+++
@@ -1,6 +0,0 @@
-// 'Implement' innerText in JSDOM: https://github.com/jsdom/jsdom/issues/1245
-Object.defineProperty(global.Element.prototype, 'innerText', {
-  get() {
-    return this.textContent;
-  },
-});
